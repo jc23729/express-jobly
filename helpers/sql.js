15452d493a2f1bc6b9ea81ee4548c6d8a6d086cc
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-05-12 19:31:29 -0400

    Update sql.js

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,4 +1,3 @@
-
 /**
  * Generate a selective update query based on a request body:
  *
@@ -36,13 +35,11 @@ function sqlForPartialUpdate(table, items, key, id) {
   let cols = columns.join(", ");
 
   let query;
-  if(table === "users"){
+  if (table === "users") {
     query = `UPDATE ${table} SET ${cols} WHERE ${key}=$${idx} RETURNING username, first_name, last_name, email, photo_url`;
-  }
-  else{
+  } else {
     query = `UPDATE ${table} SET ${cols} WHERE ${key}=$${idx} RETURNING *`;
   }
-  
 
   let values = Object.values(items);
   values.push(id);
@@ -52,7 +49,57 @@ function sqlForPartialUpdate(table, items, key, id) {
 
 module.exports = sqlForPartialUpdate;
 
+// /**
+//  * Generate a selective update query based on a request body:
+//  *
+//  * - table: where to make the query
+//  * - items: an object with keys of columns you want to update and values with
+//  *          updated values
+//  * - key: the column that we query by (e.g. username, handle, id)
+//  * - id: current record ID
+//  *
+//  * Returns object containing a DB query as a string, and array of
+//  * string values to be updated
+//  *
+//  */
 
+// function sqlForPartialUpdate(table, items, key, id) {
+//   // keep track of item indexes
+//   // store all the columns we want to update and associate with vals
+
+//   let idx = 1;
+//   let columns = [];
+
+//   // filter out keys that start with "_" -- we don't want these in DB
+//   for (let key in items) {
+//     if (key.startsWith("_")) {
+//       delete items[key];
+//     }
+//   }
+
+//   for (let column in items) {
+//     columns.push(`${column}=$${idx}`);
+//     idx += 1;
+//   }
+
+//   // build query
+//   let cols = columns.join(", ");
+
+//   let query;
+//   if(table === "users"){
+//     query = `UPDATE ${table} SET ${cols} WHERE ${key}=$${idx} RETURNING username, first_name, last_name, email, photo_url`;
+//   }
+//   else{
+//     query = `UPDATE ${table} SET ${cols} WHERE ${key}=$${idx} RETURNING *`;
+//   }
+
+//   let values = Object.values(items);
+//   values.push(id);
+
+//   return { query, values };
+// }
+
+// module.exports = sqlForPartialUpdate;
 
 //Origonal code
 // const { BadRequestError } = require("../expressError");
