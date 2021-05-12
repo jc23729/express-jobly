2c2a73668ed956ccd36371ca48359479fd3d0d56
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-05-12 19:09:38 -0400

    Update sql.js

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,6 +1,25 @@
+/**
+ * Generate a selective update query based on a request body:
+ *
+ * - table: where to make the query
+ * - items: an object with keys of columns you want to update and values with updated values
+ * - key: the column that we query by (e.g. username, handle, id)
+ * - id: current record ID
+ *
+ * Returns object containing a DB query as a string, and array of
+ * string values to be updated
+ *
+ */
 
+function sqlForPartialUpdate(table, items, key, id) {
+  // keep track of items indexes
+  //store all the columns we want to update and associated valuesIn
 
+  let idx = -1;
+  let columns = [];
 
+  
+}
 
 
 
