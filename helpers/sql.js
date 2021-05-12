1efa3cf0335e7e229de2ff960af14904df44e7e4
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-05-12 19:14:03 -0400

    Update sql.js

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,8 +1,10 @@
+
 /**
  * Generate a selective update query based on a request body:
  *
  * - table: where to make the query
- * - items: an object with keys of columns you want to update and values with updated values
+ * - items: an object with keys of columns you want to update and values with
+ *          updated values
  * - key: the column that we query by (e.g. username, handle, id)
  * - id: current record ID
  *
@@ -12,19 +14,45 @@
  */
 
 function sqlForPartialUpdate(table, items, key, id) {
-  // keep track of items indexes
-  //store all the columns we want to update and associated valuesIn
+  // keep track of item indexes
+  // store all the columns we want to update and associate with vals
 
-  let idx = -1;
+  let idx = 1;
   let columns = [];
 
-  // filter out keys that start with "_" (=> "_token") -- we don't want these in DB
+  // filter out keys that start with "_" -- we don't want these in DB
   for (let key in items) {
     if (key.startsWith("_")) {
       delete items[key];
     }
   }
+
+  for (let column in items) {
+    columns.push(`${column}=$${idx}`);
+    idx += 1;
+  }
+
+  // build query
+  let cols = columns.join(", ");
+
+  let query;
+  if(table === "users"){
+    query = `UPDATE ${table} SET ${cols} WHERE ${key}=$${idx} RETURNING username, first_name, last_name, email, photo_url`;
+  }
+  else{
+    query = `UPDATE ${table} SET ${cols} WHERE ${key}=$${idx} RETURNING *`;
+  }
+  
+
+  let values = Object.values(items);
+  values.push(id);
+
+  return { query, values };
 }
+
+module.exports = sqlForPartialUpdate;
+
+
 
 //Origonal code
 // const { BadRequestError } = require("../expressError");
