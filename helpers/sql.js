b6eeb92ad5bb7fb0e5648e6a0383ac46c670d008
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-05-12 20:32:43 -0400

    Update sql.js

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,106 +1,3 @@
-/**
- * Generate a selective update query based on a request body:
- *
- * - table: where to make the query
- * - items: an object with keys of columns you want to update and values with
- *          updated values
- * - key: the column that we query by (e.g. username, handle, id)
- * - id: current record ID
- *
- * Returns object containing a DB query as a string, and array of
- * string values to be updated
- *
- */
-
-// function sqlForPartialUpdate(table, items, key, id) {
-  // keep track of item indexes
-  // store all the columns we want to update and associate with vals
-
-//   let idx = 1;
-//   let columns = [];
-
-//   // filter out keys that start with "_" -- we don't want these in DB
-//   for (let key in items) {
-//     if (key.startsWith("_")) {
-//       delete items[key];
-//     }
-//   }
-
-//   for (let column in items) {
-//     columns.push(`${column}=$${idx}`);
-//     idx += 1;
-//   }
-
-//   // build query
-//   let cols = columns.join(", ");
-
-//   let query;
-//   if (table === "users") {
-//     query = `UPDATE ${table} SET ${cols} WHERE ${key}=$${idx} RETURNING username, first_name, last_name, email, photo_url`;
-//   } else {
-//     query = `UPDATE ${table} SET ${cols} WHERE ${key}=$${idx} RETURNING *`;
-//   }
-
-//   let values = Object.values(items);
-//   values.push(id);
-
-//   return { query, values };
-// }
-
-// module.exports = sqlForPartialUpdate;
-
-// /**
-//  * Generate a selective update query based on a request body:
-//  *
-//  * - table: where to make the query
-//  * - items: an object with keys of columns you want to update and values with
-//  *          updated values
-//  * - key: the column that we query by (e.g. username, handle, id)
-//  * - id: current record ID
-//  *
-//  * Returns object containing a DB query as a string, and array of
-//  * string values to be updated
-//  *
-//  */
-
-// function sqlForPartialUpdate(table, items, key, id) {
-//   // keep track of item indexes
-//   // store all the columns we want to update and associate with vals
-
-//   let idx = 1;
-//   let columns = [];
-
-//   // filter out keys that start with "_" -- we don't want these in DB
-//   for (let key in items) {
-//     if (key.startsWith("_")) {
-//       delete items[key];
-//     }
-//   }
-
-//   for (let column in items) {
-//     columns.push(`${column}=$${idx}`);
-//     idx += 1;
-//   }
-
-//   // build query
-//   let cols = columns.join(", ");
-
-//   let query;
-//   if(table === "users"){
-//     query = `UPDATE ${table} SET ${cols} WHERE ${key}=$${idx} RETURNING username, first_name, last_name, email, photo_url`;
-//   }
-//   else{
-//     query = `UPDATE ${table} SET ${cols} WHERE ${key}=$${idx} RETURNING *`;
-//   }
-
-//   let values = Object.values(items);
-//   values.push(id);
-
-//   return { query, values };
-// }
-
-// module.exports = sqlForPartialUpdate;
-
 // Origonal code
 const { BadRequestError } = require("../expressError");
 
@@ -111,8 +8,8 @@ function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   if (keys.length === 0) throw new BadRequestError("No data");
 
   // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
-  const cols = keys.map((colName, idx) =>
-      `"${jsToSql[colName] || colName}"=$${idx + 1}`,
+  const cols = keys.map(
+    (colName, idx) => `"${jsToSql[colName] || colName}"=$${idx + 1}`
   );
 
   return {
